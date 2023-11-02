9c8739e1a4a7cdc2418f28cf19998052d103a1b7
BourshanDor <dorbourshan@mail.tau.ac.il>
2023-11-02 17:08:50 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -4,7 +4,17 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() 
+    with conn : 
+        while True : 
+            data = conn.recv(1024)
+            if not data : 
+                break
+            conn.send('HTTP/1.1 200 OK\r\n\r\n')
+
+    
+
+
 
 
 if __name__ == "__main__":
