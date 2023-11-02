3910b8253bb1f757beef427e1e1eca70456fafd0
BourshanDor <dorbourshan@mail.tau.ac.il>
2023-11-02 17:12:47 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -6,11 +6,8 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept() 
     with conn : 
-        while True : 
-            data = conn.recv(1024)
-            if not data : 
-                break
-            conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+        data = conn.recv(1024)
+        conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
 
     
 
