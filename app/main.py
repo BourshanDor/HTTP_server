d6edf02f4a04113de4325195af0cd12af80ec655
BourshanDor <dorbourshan@mail.tau.ac.il>
2023-11-03 13:54:42 +0200

    pass 4nd stage

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ OK = b'HTTP/1.1 200 OK\r\n'
 NOT_FOUND = b'HTTP/1.1 404 Not Found\r\n'
 END = b'\r\n'
 
+
 def build_response(content : str) -> bytes : 
-    text = content[6:]
+    # text = content[6:]
     content_length = len(text)
-    text = text.encode()
+    text = content.encode()
     content_length = f'Content-Length: {content_length}\r\n\r\n'.encode()
     message = OK + b'Content-Type: text/plain\r\n' + content_length +  text + END
     return message
-
 
 
 def main():
@@ -28,18 +28,11 @@ def main():
         if len(first_line) == 3 and first_line[1] == '/' : 
             conn.send(OK + END)
         elif len(first_line) == 3 and '/echo' in first_line[1] :
-            message = build_response(first_line[1]) 
+            message = build_response((first_line[1])[6:]) 
             conn.send(message)
         else : 
             conn.send(NOT_FOUND + END)
         
-
-        
-
-    
-
-
-
 
 if __name__ == "__main__":
     main()
