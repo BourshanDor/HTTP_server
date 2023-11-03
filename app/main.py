3ae0e5e81f526caa723ffa800e32e940d4ef7dcf
BourshanDor <dorbourshan@mail.tau.ac.il>
2023-11-03 15:18:39 +0200

    pass 6nd stage

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import socket
 from typing import List
-
+import threading
 
 OK = b'HTTP/1.1 200 OK\r\n'
 NOT_FOUND = b'HTTP/1.1 404 Not Found\r\n'
 END = b'\r\n'
+
+sel = selectors.DefaultSelector()
 
 
 def build_response(content : str) -> bytes : 
@@ -20,6 +22,31 @@ def user_agent(lines : List[str]) :
             content = line.split()
             return  content[-1]
 
+def send_to_client(conn : socket) : 
+    with conn : 
+    
+        data = conn.recv(1024)
+        if b'HTTP' not in data : 
+            return  
+        lines = data.decode("utf-8").splitlines()
+        first_line = lines[0].split()
+
+        if len(first_line) == 3 and first_line[1] == '/' : 
+            message = OK + END
+            
+        elif len(first_line) == 3 and '/echo' in first_line[1] :
+            content = first_line[1]
+            content = content[6:]
+            message = build_response(content) 
+            
+        elif len(first_line) == 3 and '/user-agent' == first_line[1] :
+            content = user_agent(lines)
+            message = build_response(content) 
+
+        else : 
+            message = NOT_FOUND + END
+
+        conn.sendall(message)
 
 def main():
 
@@ -27,33 +54,11 @@ def main():
 
     while True : 
         conn, addr = server_socket.accept() 
-        with conn : 
-            
-                data = conn.recv(1024)
-                if not data:
-                    break
-                if b'HTTP' not in data : 
-                    break  
-                lines = data.decode("utf-8").splitlines()
-                first_line = lines[0].split()
+        threading.Thread(
+            target= send_to_client, args=(conn)
+        ).start()
 
-                if len(first_line) == 3 and first_line[1] == '/' : 
-                    message = OK + END
-                    
-                elif len(first_line) == 3 and '/echo' in first_line[1] :
-                    content = first_line[1]
-                    content = content[6:]
-                    message = build_response(content) 
-                    
-                elif len(first_line) == 3 and '/user-agent' == first_line[1] :
-                    content = user_agent(lines)
-                    message = build_response(content) 
 
-                else : 
-                    message = NOT_FOUND + END
-
-                conn.send(message)
-                conn.close()
-
+                
 if __name__ == "__main__":
     main()
