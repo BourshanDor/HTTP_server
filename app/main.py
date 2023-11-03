34716b6e4d018027bb22c6c1cc24a83262996c57
BourshanDor <dorbourshan@mail.tau.ac.il>
2023-11-03 17:41:32 +0200

    pass 8nd stage

--- app/main.py
+++ app/main.py
@@ -5,18 +5,22 @@ import sys
 import os 
 
 OK = b'HTTP/1.1 200 OK\r\n'
+OK201 = b'HTTP/1.1 201 OK\r\n'
 NOT_FOUND = b'HTTP/1.1 404 Not Found\r\n'
 END = b'\r\n'
 
 
 
-def build_response(content : str, content_type: str) -> bytes : 
+def build_response(content : str, content_type: str, method) -> bytes : 
     content_length = len(content)
     text = content.encode()
     content_type = content_type.encode() 
     content_type = b'Content-Type: ' + content_type + END 
     content_length = f'Content-Length: {content_length}\r\n\r\n'.encode()
-    message = OK + content_type + content_length +  text + END
+    if method == 'GET' : 
+        message = OK + content_type + content_length +  text + END
+    else : 
+        message = OK201 + content_type + content_length +  text + END
     return message
 
 def user_agent(lines : List[str]) :
@@ -42,28 +46,37 @@ def send_to_client(conn : socket, addr, directory_path) :
         elif len(first_line) == 3 and '/echo' in first_line[1] :
             content = first_line[1]
             content = content[6:]
-            message = build_response(content,'text/plain') 
+            message = build_response(content,'text/plain','GET') 
             
         elif len(first_line) == 3 and '/user-agent' == first_line[1] :
             content = user_agent(lines)
-            message = build_response(content,'text/plain') 
+            message = build_response(content,'text/plain','GET') 
 
-        elif len(first_line) == 3 and '/files' in first_line[1] and directory_path is not None: 
+        elif len(first_line) == 3 and '/files' in first_line[1] and directory_path is not None and first_line[0] == 'GET': 
             file_name = first_line[1] 
             file_name = file_name[7:]
-            print(f'{directory_path} and {file_name}')
             real_file = check_file_exists(directory_path, file_name)
 
             if real_file : 
                 path = os.path.join(directory_path, file_name)
                 with open(path, 'rb') as file:
                     content = file.read().decode()
-                    message = build_response(content,'application/octet-stream')
+                    message = build_response(content,'application/octet-stream','GET')
             else : 
 
                 message = NOT_FOUND + END
 
+        elif len(first_line) == 3 and '/files' in first_line[1] and directory_path is not None and first_line[0] == 'POST' : 
+            file_name = first_line[1] 
+            file_name = file_name[7:]
 
+            path = os.path.join(directory_path, file_name)
+            content = ''
+            with open(path, 'w') as file:
+                for i in range(1, len(lines)) : 
+                    content += file.write(lines[i]).decode()
+                message = build_response(content,'application/octet-stream','GET')
+    
         else : 
             message = NOT_FOUND + END
 
